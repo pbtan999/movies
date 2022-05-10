121e3c8ae1e8b5899a02041edfe7d684357ea2ae
pbtan999 <92437259+pbtan999@users.noreply.github.com>
2022-05-10 21:42:13 +0300

    Delete Creds.java

--- src/main/java/com/pbtan/movies/Creds.java
+++
@@ -1,6 +0,0 @@
-package com.pbtan.movies;
-
-public class Creds {
-    private String user = "ufgihxdmhdatfg";
-    private String pw = "55f9694b4e48c75a1321a45b38d2359629b7318e1add9cf8a0031cc942c65acc";
-}
